7fd5aefad69e6d96447424d10134df1172ec6314
Gökdeniz Çetin <201913709072@ogr.balikesir.edu.tr>
2023-11-28 10:29:25 +0300

    validation

--- src/components/base/ContactForm.tsx
+++ src/components/base/ContactForm.tsx
@@ -14,9 +14,16 @@ const ContactForm = (props: Props) => {
   const [loading, setLoading] = useState(false);
 
   const sendMail = async () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     setLoading(true);
     if (name === "" || phone === "" || mail === "" || message === "") {
       toast.error("Lütfen tüm alanları doldurunuz.");
+      setLoading(false);
+      return false;
+    }
+
+    if (!emailRegex.test(mail)) {
+      toast.error("Lütfen geçerli bir e-posta adresi giriniz.");
       setLoading(false);
       return false;
     }
@@ -56,21 +63,30 @@ const ContactForm = (props: Props) => {
       <Toaster position="bottom-right" />
       <div className="mt-16 max-w-2xl flex flex-col gap-4">
         <Input
+          value={name}
           onChange={(e: any) => setName(e.target.value)}
           required
+          maxLength={30}
           placeholder="Adınız ve soyadınız"
         />
         <Input
+          pattern="\d*"
+          maxLength={15}
+          value={phone}
           onChange={(e: any) => setPhone(e.target.value)}
           required
           placeholder="Telefon Numaranız"
         />
         <Input
+          maxLength={50}
+          value={mail}
           onChange={(e: any) => setMail(e.target.value)}
           required
           placeholder="Email adresiniz"
         />
         <Textarea
+          maxLength={512}
+          value={message}
           onChange={(e: any) => setMessage(e.target.value)}
           required
           placeholder="Mesajınız"
