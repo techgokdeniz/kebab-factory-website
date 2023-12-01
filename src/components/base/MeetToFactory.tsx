b48996be18d258197c3228184f4f1125d34b22d5
Gökdeniz Çetin <201913709072@ogr.balikesir.edu.tr>
2023-12-01 10:36:15 +0300

    desc rename

--- src/components/base/MeetToFactory.tsx
+++ src/components/base/MeetToFactory.tsx
@@ -31,7 +31,7 @@ const MeetToFactory = (props: Props) => {
             gelen kendine özgü bir tada sahiptir.
           </p>
         </div>
-        <div className="grid grid-cols-1  lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1  lg:grid-cols-2 gap-4">
           {MeetItems.map((item, index) => (
             <ProductsCard key={index} icon={<item.icon />} desc={item.text} />
           ))}
